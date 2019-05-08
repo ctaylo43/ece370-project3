6d63a4390c0d77bb4441aca6929a89b8ef62d718
Caleb Taylor <ctaylo43@gmu.edu>
2019-05-08 04:40:02 -0400

    controller working, need to change ip address back

--- pi_controller.py
+++ pi_controller.py
@@ -5,7 +5,7 @@ import time
 from ctypes import *
 from getkey import getkey, keys
 
-UDP_IP = "192.168.1.1"
+UDP_IP = "127.0.0.1"
 recPort = 5005;
 sendPort = 4242;
 
@@ -27,7 +27,11 @@ sendsock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sendsock.bind((UDP_IP, sendPort))
 
-print "Robot Controls"
+print ""
+print "###############################"
+print ""
+print "     Robot Controls"
+print ""
 print "UP : increase speed"
 print "DOWN : decrease speed"
 print "RIGHT : rotate clockwise"
@@ -38,17 +42,19 @@ print "s : rotate to south"
 print "d : rotate to east"
 print "space bar : stop robot"
 print "q : exit program"
+print ""
+print "###############################"
 
 while True:
 	input = getkey()
 	
 	if (input == keys.UP):
-		v += 25.5	# 255 / 10 = 25.5 for 10 increments to max speed
+		v += 25 	# 255 / 10 = 25.5 for 10 increments to max speed
 		send = True
 		send_in = "UP"
 
 	elif (input == keys.DOWN):
-		v -= 25.5	# 255 / 10 = 25.5 for 10 increments to max speed
+		v -= 25 	# 255 / 10 = 25.5 for 10 increments to max speed
 		send = True
 		send_in = "DOWN"
 
@@ -85,18 +91,21 @@ while True:
 	elif (input == keys.SPACE):
 		v = 0
 		send = True
-		send_in = "d"
+		send_in = "space"
 
 	elif (input == 'q'):
 		print "Exiting program"
 		sys.exit()
 
-	if v > 255: v = 255
-	if v < 0: v = 0
+	if v > 250: v = 250
+	if v < -250: v = -250
 	if theta > 360: theta = 360
 	if theta < 0: theta = 0
 
 	if (send == True):
-		vel = Velocity(v,theta)
-		print "Sending input " + send_in
+		vel = Velocity(c_int(v),c_int(theta))
+		print " "
+                print "v = " + str(v)
+                print "theta = " + str(theta)
+                print "Sending input " + send_in
 		sendsock.sendto(vel, (UDP_IP, sendPort))
